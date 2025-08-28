f2f0106f4f146b39aa901edac51d33619d709885
Vignesh Pathak <vigneshpathakdev@gmail.com>
2025-08-27 22:28:08 -0500

    Commneted unneccary blackage again

--- src/lib/integrations/index.ts
+++ src/lib/integrations/index.ts
@@ -2,21 +2,17 @@
 
 import { IntegrationRegistry } from './registry'
 import { IntegrationManager } from './manager'
-import { GitHubIntegration } from './providers/github'
-import { GoogleIntegration } from './providers/google'
-import { MicrosoftIntegration } from './providers/microsoft'
-import { SlackIntegration } from './providers/slack'
+
 // Import other integrations as you create them
 
-// Initialize and register all integrations
 export function initializeIntegrations() {
   const registry = IntegrationRegistry.getInstance()
   
   // Register all available integrations
-  registry.register(new GoogleIntegration())
-  registry.register(new SlackIntegration())
-  registry.register(new GitHubIntegration())
-  registry.register(new MicrosoftIntegration())
+  // registry.register(new GoogleIntegration())
+  // registry.register(new SlackIntegration())
+  // registry.register(new GitHubIntegration())
+  // registry.register(new MicrosoftIntegration())
   
   // Add more integrations here as you build them
   // registry.register(new GithubIntegration())
